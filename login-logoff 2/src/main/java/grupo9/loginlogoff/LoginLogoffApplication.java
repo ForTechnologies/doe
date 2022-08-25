d0578fbbd10d9cfac6f4869f94ffff2992a60f42
raylanef <raylanefernandes@hotmail.com>
2022-08-25 12:58:10 -0300

    initial commit

---
+++ login-logoff 2/src/main/java/grupo9/loginlogoff/LoginLogoffApplication.java
@@ -0,0 +1,13 @@
+package grupo9.loginlogoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginLogoffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginLogoffApplication.class, args);
+	}
+
+}
