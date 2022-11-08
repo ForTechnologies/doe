2d8de6929115f09e5343da80d8b122d50c5abcf1
raylanef <raylanefernandes@hotmail.com>
2022-11-07 21:08:26 -0300

    ajuste pastas

--- back-end/Doe/src/main/java/br/com/doe/controller/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.doe.controller.exception;
-
-public class NotFoundException extends RuntimeException{
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
--- back-end/Doe/src/main/java/br/com/doe/repositories/PermissaoUsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.doe.repositories;
-
-import br.com.doe.controller.dtos.PermissaoDto;
-import br.com.doe.entities.Permissao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PermissaoUsuarioRepository extends JpaRepository<Permissao,Integer> {
-}
