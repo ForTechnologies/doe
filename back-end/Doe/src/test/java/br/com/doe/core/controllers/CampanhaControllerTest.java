7f2c270191ef5ff46326dea3c7930f17aea6fe6a
raylanef <raylanefernandes@hotmail.com>
2022-11-28 21:38:18 -0300

    add testes controller campanhas

--- back-end/Doe/src/test/java/br/com/doe/core/controllers/CampanhaControllerTest.java
+++ back-end/Doe/src/test/java/br/com/doe/core/controllers/CampanhaControllerTest.java
@@ -1,10 +1,20 @@
 package br.com.doe.core.controllers;
 
 
+import br.com.doe.core.dtos.CampanhaDto;
+import br.com.doe.core.entities.Campanha;
 import br.com.doe.core.services.CampanhaService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
 
 
 @SpringBootTest(classes = CampanhaController.class)
@@ -14,5 +24,31 @@ class CampanhaControllerTest {
 
     @MockBean
     CampanhaService service;
+
+
+    @Test
+    void get200ComCorpo(){
+        int idTeste = 12;
+        List<CampanhaDto> lista = List.of(
+                mock(CampanhaDto.class),
+                mock(CampanhaDto.class)
+        );
+
+        when(service.buscarCampanhasPorOng(idTeste)).thenReturn(lista);
+        ResponseEntity<List<CampanhaDto>> respostaObtida = controller.buscaCampanhasPorOng(12);
+
+        assertEquals(200, respostaObtida.getStatusCodeValue());
+        assertNotNull(respostaObtida.getBody());
+    }
+
+    @Test
+    void put200ComCorpo(){
+        CampanhaDto campanhaTeste = new CampanhaDto();
+        when(service.atualizar(12, campanhaTeste)).thenReturn(campanhaTeste);
+        ResponseEntity<CampanhaDto> respostaObtida = controller.atualizaCampanha(12, campanhaTeste);
+
+        assertEquals(200, respostaObtida.getStatusCodeValue());
+        assertNotNull(respostaObtida.getBody());
+    }
 
 }
